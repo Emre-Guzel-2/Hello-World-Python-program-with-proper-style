867cf902e44f0f0c7c0766e3df7ed763ce8b3ff6
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2026-09-10 15:41:55 +0000

    added hello world program in c

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+// Copyright 2026 Emre Guzel
+//
+// Created by: Emre Guzel
+// Created on: Sept 10, 2026
+// This is the "Hello, World!" module, with proper style
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World\n");
+    return 0;
+}
