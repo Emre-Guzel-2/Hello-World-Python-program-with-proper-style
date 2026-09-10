6fb70d1579203a995312efe21fb634f0a65d6b01
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2026-09-10 15:09:59 +0000

    added hello world program

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Created by: Emre Guzel
+Created on: Sept 10, 2026
+This is the "Hello, World!" module, with proper style
+"""
+
+
+def main() -> None:
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
